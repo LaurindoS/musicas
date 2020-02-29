b86260bb2d931fcb8c0c999793529e89cb8245e7
r-paulo-santos <Paulo.Ricardo_santos@outlook.com>
2020-02-28 21:45:21 -0300

    Foi adicionado o que o exercício pediu, no caso, a letra da música, com cores variadas.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Musicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Vai se entregar pra mim");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Como a primeira vez");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Vai delirar de amor");
+            Console.WriteLine("Sentir o meu calor");
+            Console.WriteLine("Vai me pertencer");
+            Console.ForegroundColor = ConsoleColor.Blue;;
+            Console.WriteLine("Sou pássaro de fogo");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
+
